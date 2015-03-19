c394cae4f02cb3b5527c80fe59062d3680790e66
Shivaal Roy <shivaal@stanford.edu>
2015-03-19 13:56:19 -0700

    Trying to fix database connection

--- db_conx.php
+++ db_conx.php
@@ -3,6 +3,7 @@ $host = getenv("HOST");
 $dbname = getenv("DATABASE");
 $user = getenv("USER");
 $password = getenv("PASSWORD");
+echo $conn_string;
 $conn_string = "host=".$host." dbname=".$dbname." user=".$user." password=".$password;
 $db_conx = pg_connect($conn_string);
 // Evaluate the connection
